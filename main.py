d057bf51ab63e1b66aa5d335e133476e9b46030d
LenaNS <lena.noskova.99@mail.ru>
2024-07-18 23:38:52 +0800

    +

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql import functions as F
+
 
 # Инициализация SparkSession
-# spark = SparkSession.builder.appName("ProductCategory").getOrCreate()
-# spark = SparkSession.builder.master("local[*]").appName('ProductCategory').getOrCreate()
 spark = SparkSession.Builder().appName('ProductCategory').getOrCreate()
 
-print(spark.version)
 # Пример данных
 products_data = [
     (1, "Продукт A"),
@@ -28,7 +25,6 @@ product_categories_data = [
 ]
 
 # Создание датафреймов
-
 products_df = spark.createDataFrame(products_data, ["product_id", "product_name"])
 categories_df = spark.createDataFrame(categories_data, ["category_id", "category_name"])
 product_categories_df = spark.createDataFrame(product_categories_data, ["product_id", "category_id"])
@@ -48,18 +44,10 @@ def get_product_category_pairs_and_orphan_products(products_df: DataFrame, produ
         "product_name",
         "category_name"
     )
-
-    # Продукты без категорий
-    orphan_products = products_df.join(
-        product_categories_df,
-        "product_id",
-        "left_anti"
-    ).select("product_name")
-
-    return product_category_pairs, orphan_products
+    return product_category_pairs
 
 # Вызов метода
-product_category_pairs, orphan_products = get_product_category_pairs_and_orphan_products(products_df, product_categories_df)
+product_category_pairs = get_product_category_pairs_and_orphan_products(products_df, product_categories_df)
 
 # Показать результат
 print("Пары «Имя продукта – Имя категории»: ")
